9b317206d59aa8f323352981e55f7dab14258ef1
SaraG94 <sara.gherbassi.94@gmail.com>
2023-04-05 16:17:26 +0200

    FAET second route

--- resources/views/layouts/comics.blade.php
+++ resources/views/layouts/comics.blade.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dc home</title>
+    <title>Comics</title>
 </head>
 <body>
     @include('partials.header')
 
     <main>
-        contenuto...
+        contenuto 2...
     </main>
 
     @include('partials.footer')
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('layouts.app');
 });
+
+Route::get('/comics', function () {
+    return view('layouts.comics');
+});
