36f4021fa1fddb7084646250f4aa1d58eebbf563
SaraG94 <sara.gherbassi.94@gmail.com>
2023-04-05 19:19:01 +0200

    FEAT footer text + style

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -7,8 +7,101 @@
     <title>Document</title>
 </head>
 <body>
- <footer>
-    <h1>questo è il footer</h1>
- </footer>
+    <footer>
+        
+        <section class="bg">
+            <div class="container footer_menu">
+                <div class="menu-items">
+                    <div class="col">
+                        <h3>dc comics</h3>
+                        <ul>
+                            <li><a href="#"> Characters</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">TV</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">News</a></li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <h3>Shop</h3>
+                        <ul>
+                            <li><a href="#">Shop Dc</a></li>
+                            <li><a href="#">Shop Dc Colllectibles</a></li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <h3>Dc</h3>
+                        <ul>
+                            <li><a href="#">Terms Of Use</a></li>
+                            <li><a href="#">Privacy policy(new)</a></li>
+                            <li><a href="#">Ad Choices</a></li>
+                            <li><a href="#">Advertising</a></li>
+                            <li><a href="#">jobs</a></li>
+                            <li><a href="#">Subscriptions</a></li>
+                            <li><a href="#">Talent Workshops</a></li>
+                            <li><a href="#">CPSC Certificates</a></li>
+                            <li><a href="#">Ratings</a></li>
+                            <li><a href="#">Shop Help</a></li>
+                            <li><a href="#">contact Us</a></li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <h3>Sites</h3>
+                        <ul>
+                            <li><a href="#">DC</a></li>
+                            <li><a href="#">MAD Magazine</a></li>
+                            <li><a href="#">Dc Kids</a></li>
+                            <li><a href="#">Dc Universe</a></li>
+                            <li><a href="#">Dc Power visa</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+
+                <div class="big-logo"></div>
+
+                <div class="col-6 text-copy">
+                    <p>
+                        All Site content TM and c 2020 DC Entertainment, unless otherwise
+                        <a href="#" class="blue">noted here</a>. All right reserved.
+                        <a href="#" class="blue">Cookies Settings</a>
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <section class="contact-us">
+            <div class="container">
+                <div class="row">
+                    <div class="col sing">
+                        <button>Sing-Up now!</button>
+                    </div>
+
+                    <div class="col social">
+                        <h2>Follow us</h2>
+                        <ul>
+                            <li>
+                                <img src="{{Vite::asset('resources/img/footer-facebook.png')}}" alt="">
+                            </li>
+                            <li>
+                                <img src="{{Vite::asset('resources/img/footer-twitter.png')}}" alt="">
+                            </li>
+                            <li>
+                                <img src="{{Vite::asset('resources/img/footer-youtube.png')}}" alt="">
+                            </li>
+                            <li>
+                                <img src="{{Vite::asset('resources/img/footer-pinterest.png')}}" alt="">
+                            </li>
+                            <li>
+                                <img src="{{Vite::asset('resources/img/footer-periscope.png')}}" alt="">
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </footer>
 </body>
 </html>
