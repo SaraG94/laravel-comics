e1048e3b8b55f4ff9ec90d24d3568497fae3098e
SaraG94 <sara.gherbassi.94@gmail.com>
2023-04-05 16:43:12 +0200

    FEAT yield('page-title')

--- resources/views/layouts/comics.blade.php
+++ resources/views/layouts/comics.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.app')
 
+<!--assegno il valore Comics al titolo della pagina -->
+@section('page-title','Comics')
+
 @section('content')
 <h1>questa è la pagina comics</h1>
 @endsection
--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.app')
 
+<!--assegno il valore home al titolo della pagina -->
+@section('page-title','Home')
+
 @section('content')
 <h1>questa è la home</h1>
 @endsection
